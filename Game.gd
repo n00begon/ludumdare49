cf45f35b63ed82550cabe79a2c3ba43aee2b371d
Stepan Belousov <stepan.belousov@tracmap.com>
2021-10-02 14:38:47 +1300

    Add deer counter

--- Game.gd
+++ Game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const MIN_SPAWN_INTERVAL_MS = 300
+
 var muskDeer = load("res://MuskDeer.tscn")
 var rng = RandomNumberGenerator.new()
 
@@ -7,17 +9,18 @@ var rng = RandomNumberGenerator.new()
 func _ready():
 	pass # Replace with function body.
 
-const MIN_SPAWN_INTERVAL_MS = 300
+var deerCount = 0
 var lastSpawnTime = 0
 
 func _spawn_deer():
 		var newDeer = muskDeer.instance()
 		var viewport = get_viewport().size
 		newDeer.set_global_position(Vector2(rng.randi_range(0, viewport.x), rng.randi_range(0, viewport.y)))
-		get_parent().add_child(newDeer)	
+		get_parent().add_child(newDeer)
+		deerCount += 1
 
 func _process(delta):
-	# Quit on ESC
+	# Quit on ESC or Q
 	if Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("ui_quit"):
 		get_tree().quit()
 
