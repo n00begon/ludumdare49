499d788d19f4a94769c732a7ff05d6ecc742bafe
Stepan Belousov <stepan.belousov@tracmap.com>
2021-10-02 17:53:15 +1300

    Randomize plants

--- Game.gd
+++ Game.gd
@@ -34,6 +34,8 @@ func _spawn(type, name, scene, actionKey, forceSpawn):
 		return
 
 	var newObj = scene.instance()
+	if type == 'plant':
+		newObj.get_node("Sprite").texture = load("res://assets/plants/" + str(rng.randi() % 7 + 1) + ".png")
 	var prefix = type + "_" + name
 	newObj.name = prefix + "_" + str(counters.get(prefix, 0))
 	print("Spawn: " + newObj.name)
