bf770f7eb3e8bac2425a104d5a54f691ba6bcaae
Tim Neumegen <tneumegen@gmail.com>
2021-10-02 13:43:24 +1300

    First cut at eating a bush

---
+++ MuskDeer.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var run_speed = 250
+var velocity = Vector2.ZERO
+var food = null
+var rng = RandomNumberGenerator.new()
+var walkCount = 100
+
+func _physics_process(delta):
+	if food:
+		velocity = position.direction_to(food.position) * run_speed
+	else:
+		walkCount -= 1
+		if walkCount < 0:
+			walkCount = 100
+			velocity = Vector2(rng.randf_range(-run_speed, run_speed), rng.randf_range(-run_speed, run_speed))
+	move_and_slide(velocity)
+
+func _on_VisionArea_body_entered(body):
+	food = body
+
+func _on_VisionArea_body_exited(body):
+	food = null
