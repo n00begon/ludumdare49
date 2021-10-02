6a9e05259dac8ab650ba335ddf1e00e2a7fdda8b
Stepan Belousov <stepan.belousov@tracmap.com>
2021-10-02 14:31:50 +1300

    Move spawn to a separate function

--- Game.gd
+++ Game.gd
@@ -10,11 +10,14 @@ func _ready():
 const MIN_SPAWN_INTERVAL_MS = 300
 var lastSpawnTime = 0
 
+func _spawn_deer():
+		var newDeer = muskDeer.instance()
+		var viewport = get_viewport().size
+		newDeer.set_global_position(Vector2(rng.randi_range(0, viewport.x), rng.randi_range(0, viewport.y)))
+		get_parent().add_child(newDeer)	
+
 func _process(delta):
-	if Input.is_action_pressed("ui_deer"):
-		if OS.get_ticks_msec() - lastSpawnTime > MIN_SPAWN_INTERVAL_MS:			
-			var newDeer = muskDeer.instance()
-			var viewport = get_viewport().size
-			newDeer.set_global_position(Vector2(rng.randi_range(0, viewport.x), rng.randi_range(0, viewport.y)))
-			get_parent().add_child(newDeer)
+	if OS.get_ticks_msec() - lastSpawnTime > MIN_SPAWN_INTERVAL_MS:
+		if Input.is_action_pressed("ui_deer"):
+			_spawn_deer()
 			lastSpawnTime = OS.get_ticks_msec()
