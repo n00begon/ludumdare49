063cfc7b5845294c0c999e459dd388ec17fb1fca
leonfromont <leon.fromont@gmail.com>
2021-10-02 22:55:49 +1300

    plants have debug as well

--- plant_bush.gd
+++ plant_bush.gd
@@ -2,8 +2,19 @@ extends KinematicBody2D
 
 var rng = RandomNumberGenerator.new()
 var health = 100
+var debug = true
 
 # TODO(Leon): restore our health over time (but capped at health max)
+func _physics_process(delta):
+	if debug:
+		var label = self.get_child(3)
+		var labels = PoolStringArray([
+			"health : %s"
+		])
+
+		var text = labels.join("\n")
+		var label_str = text % [health]
+		label.set_text(label_str)
 
 func _on_EatRange_body_entered(body):
 	pass
