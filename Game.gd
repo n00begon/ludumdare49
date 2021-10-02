fd3996a024f3f780dc07b734ef15d97ed28910c8
Stepan Belousov <int128t@gmail.com>
2021-10-03 12:28:26 +1300

    Add dead animals

--- Game.gd
+++ Game.gd
@@ -8,6 +8,7 @@ var bearScene = load("res://life/pred/pred_bear.tscn")
 var bushScene = load("res://life/plant/plant_bush.tscn")
 var grassScene = load("res://life/plant/plant_grass.tscn")
 var treeScene = load("res://life/plant/plant_tree.tscn")
+var deadScene = load("res://dead/dead_animal.tscn")
 var rng = RandomNumberGenerator.new()
 
 var counters = {}
@@ -22,6 +23,13 @@ func _spawn_moose(forceSpawn):
 func _spawn_bear(forceSpawn):
 	_spawn("pred", "bear", bearScene, "ui_bear", forceSpawn)
 
+func _spawn_dead(name, position):
+	var newObj = deadScene.instance()
+	var texture = load("res://assets/animals/dead/" + name + ".png")
+	newObj.set_global_position(position)
+	newObj.texture = texture
+	self.add_child(newObj)
+
 func _spawn_plant(forceSpawn):
 	var plant_type = rng.randi_range(1, 3)
 	match plant_type:
@@ -31,6 +39,7 @@ func _spawn_plant(forceSpawn):
 			_spawn("plant", "grass", grassScene, "ui_plant", forceSpawn)
 		3:
 			_spawn("plant", "tree", treeScene, "ui_plant", forceSpawn)
+
 func _spawn(type, name, scene, actionKey, forceSpawn):
 	if not Input.is_action_pressed(actionKey) and not forceSpawn:
 		return
