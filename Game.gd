17d90abdc951839bd579d17772a493129cb0bd29
Stepan Belousov <stepan.belousov@tracmap.com>
2021-10-02 15:02:20 +1300

    Spawn bears

--- Game.gd
+++ Game.gd
@@ -3,9 +3,11 @@ extends Node2D
 const MIN_SPAWN_INTERVAL_MS = 300
 
 var muskDeer = load("res://MuskDeer.tscn")
+var bearScene = load("res://Bear.tscn")
 var rng = RandomNumberGenerator.new()
 
 var deerCount = 0
+var bearCount = 0
 var lastSpawnTime = 0
 
 func _ready():
@@ -20,6 +22,15 @@ func _spawn_deer():
 		get_parent().add_child(newDeer)
 		deerCount += 1
 
+func _spawn_bear():
+		var newBear = bearScene.instance()
+		newBear.name = "pred_bear_" + str(bearCount)
+		var viewport = get_viewport().size
+		newBear.set_global_position(Vector2(rng.randi_range(0, viewport.x), rng.randi_range(0, viewport.y)))
+		print(newBear.name)
+		get_parent().add_child(newBear)
+		bearCount += 1
+
 func _process(delta):
 	# Quit on ESC or Q
 	if Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("ui_quit"):
@@ -28,4 +39,7 @@ func _process(delta):
 	if OS.get_ticks_msec() - lastSpawnTime > MIN_SPAWN_INTERVAL_MS:
 		if Input.is_action_pressed("ui_deer"):
 			_spawn_deer()
+			lastSpawnTime = OS.get_ticks_msec()
+		elif Input.is_action_pressed("ui_bear"):
+			_spawn_bear()
 			lastSpawnTime = OS.get_ticks_msec()
