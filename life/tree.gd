e073c122547e6b66643796b93311dba44fc4c365
Stepan Belousov <int128t@gmail.com>
2021-10-03 16:08:46 +1300

    Fix tree texture randomization

--- life/tree.gd
+++ life/tree.gd
@@ -4,10 +4,12 @@ func _init():
 	self.species = 'tree'
 	self.is_eaten_by = ['moose']
 	self.scene = load("res://life/tree.tscn")
-	var choose = self.rng.randi() % 3
+	# For some weird reason, randomize() doesn't work and we're always getting the same random number
+	# here, so I replaced it with OS.get_time().second
+	var choose = OS.get_time().second % 3
 	var texture = null
 	match choose:
-		1: texture = load("res://assets/plants/sapling.png")
-		2: texture = load("res://assets/plants/tree-1.png")
-		3: texture = load("res://assets/plants/tree-2.png")
+		0: texture = load("res://assets/plants/sapling.png")
+		1: texture = load("res://assets/plants/tree-1.png")
+		2: texture = load("res://assets/plants/tree-2.png")
 	self.get_node("Sprite").texture = texture
