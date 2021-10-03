d020c2fc2415b8eae9aa84985ca7eb1bb5806dfa
Stepan Belousov <int128t@gmail.com>
2021-10-03 17:32:51 +1300

    Only quit on ESC

--- Game.gd
+++ Game.gd
@@ -21,8 +21,8 @@ func _ready():
 	OS.window_fullscreen = true
 
 func _process(delta):
-	# Quit on ESC or Q
-	if Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("ui_quit"):
+	# Quit on ESC
+	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
 	_spawn(mooseScene, "ui_moose")
