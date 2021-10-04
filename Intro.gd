47b4103a3a9d4cfcad0d097b1952e053dbce17f5
Tim Neumegen <tneumegen@gmail.com>
2021-10-05 07:25:14 +1300

    Intro Scene Layout

--- Intro.gd
+++ Intro.gd
@@ -1,16 +1,9 @@
-extends Node2D
+extends CenterContainer
 
 func _ready():
 	OS.window_fullscreen = true
-	#VisualServer.set_default_clear_color(Color(0.2,0.2,0.2,1))
 
 func _process(delta):
-	var stable_button = get_node("Stable")
-	var sandbox_button = get_node("Sandbox")
-	var instructions = get_node("Instructions")
-	stable_button.set_global_position(get_viewport().size / 2 - stable_button.rect_size / 2)
-	instructions.set_global_position(get_viewport().size / 2 - instructions.rect_size / 2 - Vector2(0, stable_button.rect_size.y))
-	sandbox_button.set_global_position(get_viewport().size / 2 - stable_button.rect_size / 2 + Vector2(0, stable_button.rect_size.y))
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
